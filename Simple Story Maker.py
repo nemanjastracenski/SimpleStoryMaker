a75e7beaab31fd8b4225f98a3bae1373120e08b4
Nemanja Stracenski <69650865+nemanjastracenski@users.noreply.github.com>
2021-03-17 17:14:46 +0100

    Update Simple Story Maker.py

--- Simple Story Maker.py
+++ Simple Story Maker.py
@@ -1,23 +1,53 @@
 import time
-print("What's your name?")
-character_name = input("Type it in here: ")
-print("How old are you?")
-character_age = input("Type your age in here here: ")
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+print(Fore.RED + Back.WHITE + "What's your name?")
+character_name = input(Fore.RED + "Type it in here: ")
+
+print(Fore.RED + Back.WHITE + "How old are you?")
+character_age = input(Fore.RED + "Type your age in here here: ")
+
+print(Fore.RED + Back.WHITE + "Where are you from?")
+character_whereabouts = input(Fore.RED + "Type the country in here: ")
+
+
 def isMale():
-    Male = input("You're a male? True or False? ")
+    Male = input(Fore.RED + Back.WHITE + "You're a male? True or False?: ")
     if (Male == "True"):
         Male_Bool = True
     elif (Male == "False"):
         Male_Bool = False
     if (Male_Bool) == True:
-        print("There was a man named " + character_name + ", ")
-        print("he was " + character_age + ".")
-        print("He likes the name " + character_name + ", ")
-        print("but didn't like being " + character_age + ".")
-    elif(Male_Bool) == False:
-        print("There was a woman named " + character_name + ", ")
-        print("she was " + character_age + ".")
-        print("She likes the name " + character_name + ", ")
-        print("but didn't like being " + character_age + ".")
+        print(Fore.BLUE + "There was a man named " + character_name + ", ")
+        print(Fore.BLUE + "he was " + character_age + ".")
+        print(Fore.BLUE + "He likes the name " + character_name + ", ")
+        print(Fore.BLUE + "but didn't like being " + character_age + ".")
+        print(Fore.BLUE + "He comes from " + character_whereabouts + ",")
+    elif (Male_Bool) == False:
+        print(Fore.BLUE + "There was a woman named " + character_name + ", ")
+        print(Fore.BLUE + "she was " + character_age + ".")
+        print(Fore.BLUE + "She likes the name " + character_name + ", ")
+        print(Fore.BLUE + "but didn't like being " + character_age + ".")
+        print(Fore.BLUE + "She comes from " + character_whereabouts + ",")
+
+
+def likesCountry():
+    print("Do you like your country?")
+    likes = input(Fore.RED + Back.WHITE + "True or False?: ")
+    if (likes == "True"):
+        likes_Bool = True
+    elif (likes == "False"):
+        likes_Bool = False
+    if (likes_Bool) == True:
+        print(Fore.BLUE + "and likes the country.")
+    elif (likes_Bool) == False:
+        print(Fore.BLUE + "and doesn't like the country.")
+
+
 isMale()
+
+likesCountry()
+
 time.sleep(10)
